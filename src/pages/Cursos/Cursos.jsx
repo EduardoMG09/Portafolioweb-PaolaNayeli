519b259092666d41e9e3fe34501d396d1fcb3493
Eduardo Guerrero <lalito2003mg@gmail.com>
2026-01-11 00:16:34 -0600

    pagina de cursos

--- src/pages/Cursos/Cursos.jsx
+++ src/pages/Cursos/Cursos.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import "./Cursos.css";
+import img from "../../assets/proximamente.png";
 
 function Cursos() {
   return (
-    <div>
-      <h1>Cursos</h1>
-      <p>Esta es la página de Cursos.</p>
+    <div className="cursos-container">
+      <div className="cursos-content">
+        <img src={img} alt="Próximamente" className="cursos-image" />
+        <p className="cursos-text">
+          ¡Estamos trabajando para traerte una selección de cursos
+          interesantes! Pronto podrás acceder a contenido educativo de alta
+          calidad en diversas áreas. Mantente atento a las actualizaciones.
+        </p>
+      </div>
     </div>
   );
 }
